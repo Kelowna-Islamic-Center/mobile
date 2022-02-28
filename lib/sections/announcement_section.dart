150990cd65eb06a88ef10a260c4c2d059082bd8d
Musab Hassan <musabhassan04@gmail.com>
2022-02-28 13:59:38 -0800

    Basic bottom navigation

---
+++ lib/sections/announcement_section.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AnnouncementsPage extends StatelessWidget {
+  const AnnouncementsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) =>
+      const Scaffold(body: Center(child: Text("Announcements")));
+}
---
+++ lib/sections/prayer_section.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class PrayerPage extends StatelessWidget {
+  const PrayerPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) =>
+      const Scaffold(body: Center(child: Text("Prayer")));
+}
