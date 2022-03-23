cbb4dc26188a92cc9fc23804b2deb1835001c8e1
Musab Hassan <musabhassan04@gmail.com>
2022-03-23 10:35:54 -0700

    Create basic layout of admin tools

--- lib/admin-tools/admin_page.dart
+++ lib/admin-tools/admin_page.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:kelowna_islamic_center/admin-tools/announcements_editor.dart';
+import 'package:kelowna_islamic_center/admin-tools/prayer_editor.dart';
 
 class AdminPage extends StatelessWidget {
   const AdminPage({Key? key}) : super(key: key);
 
+  // TODO: Add offline support
+
   @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      title: const Text("Admin Tools")
-    ),
-    body: const Center(child: Text("Admin Tools"))
-  );
+  Widget build(BuildContext context) => DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Admin Tools"),
+          bottom: const TabBar(tabs: [
+            Tab(text: "Prayer Times"),
+            Tab(text: "Announcements")
+          ]),
+        ),
+        body: const TabBarView(children: [
+          PrayerEditor(),
+          AnnouncementsEditor()
+        ])
+      )
+    );
 }
---
+++ lib/admin-tools/announcements_editor.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AnnouncementsEditor extends StatelessWidget {
+  const AnnouncementsEditor({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) =>
+      const Scaffold(body: Text("Announcements Editor"));
+}
