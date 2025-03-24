bc42d7c3f1998c17b487acc435b5d098f99688ba
Musab Hassan <musabhassan04@gmail.com>
2025-03-23 20:08:35 -0700

    Revert debug logs and increment version number

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,7 @@ Future<void> main() async {
 
   // Initialize app services
   await Alarm.init();
-  await Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
+  await Workmanager().initialize(callbackDispatcher, isInDebugMode: false);
   await ApiFetchService.initBackgroundService();
   await PrayerNotificationService.initBackgroundService();
 
